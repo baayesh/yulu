cc9d108890b25cb52a2af2b3984732e8a26aef63
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-08 13:56:11 +0530

    add common styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="paragraph container bg__4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi earum ea et, nisi praesentium quisquam voluptas! Quisquam quis eaque soluta veniam perspiciatis, ipsam a temporibus quam recusandae exercitationem minima doloremque incidunt facere ad cum dignissimos ullam distinctio! Atque cum dolorum earum rerum deserunt tempora officia voluptates ipsam quaerat porro accusamus reprehenderit minima ab dicta itaque sequi exercitationem, praesentium eveniet voluptas, doloribus autem, asperiores corrupti dolore. Quo, repudiandae impedit error quas, ducimus repellendus sequi deleniti modi dolorem quisquam soluta velit? Quidem assumenda qui hic commodi et dolores aspernatur esse laudantium quo illo, optio inventore, odio minima laborum voluptate dolorum ad recusandae.</h1>
     </div>
   );
 }
