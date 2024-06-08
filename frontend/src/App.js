02061f637ed46ad4490ba0c7caf92327270b668c
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-08 17:16:24 +0530

    create header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-        <h1 className="paragraph container bg__4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi earum ea et, nisi praesentium quisquam voluptas! Quisquam quis eaque soluta veniam perspiciatis, ipsam a temporibus quam recusandae exercitationem minima doloremque incidunt facere ad cum dignissimos ullam distinctio! Atque cum dolorum earum rerum deserunt tempora officia voluptates ipsam quaerat porro accusamus reprehenderit minima ab dicta itaque sequi exercitationem, praesentium eveniet voluptas, doloribus autem, asperiores corrupti dolore. Quo, repudiandae impedit error quas, ducimus repellendus sequi deleniti modi dolorem quisquam soluta velit? Quidem assumenda qui hic commodi et dolores aspernatur esse laudantium quo illo, optio inventore, odio minima laborum voluptate dolorum ad recusandae.</h1>
+      <Home />
     </div>
   );
 }
---
+++ frontend/src/components/common/header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../styles/header__footer.css";
+// Make navigation align to right side
+
+const header = () => {
+  return (
+    <section>
+      <div className="column header_linear__gradient">
+        <div className="container row  header__top__bar">
+          <h3 className="main__heading__2">YULU RENTER CAR</h3>
+        </div>
+        <div className="row container nav__container">
+          {/* <img src={logo} alt="logo" className="logo" /> */}
+          <nav className="nav">
+            <div className="nav__list">
+              <a href="/" className="nav__link tag__1">
+                Home
+              </a>
+              <a href="/about" className="nav__link tag__1">
+                About&nbsp;Us
+              </a>
+              <a href="/contact" className="nav__link tag__1">
+                Contact&nbsp;Us
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default header;
