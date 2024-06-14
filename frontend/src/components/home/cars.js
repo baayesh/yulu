f48c58341382e73949dc969f4bd53821bb5c85ba
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-14 17:07:52 +0530

    change into grid our services

--- frontend/src/components/home/cars.js
+++ frontend/src/components/home/cars.js
@@ -5,8 +5,8 @@ import { FaGasPump } from "react-icons/fa";
 const Cars = () => {
   return (
     <section>
-      <div className="column container cars__main__container">
-        <div className="row cars__row">
+      <div className=" wrapper__grid container cars__main__container">
+        {/* <div className=" cars__row"> */}
           <div className="column individual__car">
             <img src={carImg} alt="" />
             <h4 className="subheading__3"> Car Name</h4>
@@ -32,8 +32,8 @@ const Cars = () => {
             <p className="tag__2">Car Brand</p>
             <p className="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis dolore cupiditate totam ab nam.</p>
           </div>
-        </div>
-        <div className="row cars__row">
+        {/* </div> */}
+        {/* <div className="row cars__row"> */}
           <div className="column individual__car">
             <img src={carImg} alt="" />
             <h4 className="subheading__3"> Car Name</h4>
@@ -58,7 +58,7 @@ const Cars = () => {
             <p className="tag__2">Car Brand</p>
             <p className="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis dolore cupiditate totam ab nam.</p>
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </section>
   );
--- frontend/src/components/home/ourServices.js
+++ frontend/src/components/home/ourServices.js
@@ -6,10 +6,10 @@ import S3Img from '../../assets/home/Our Services/S3.png';
 const OurServices = () => {
   return (
     <section>
-        <div className="column container our__services__container">
+        <div className=" container our__services__container">
             <h2 className='tag__1'>Our Services</h2>
             <h4 className='subheading__2'>Our Best Services for You</h4>
-            <div className="row our__services__cards">
+            <div className="our__services__cards">
                 <div className="column our__services__card">
                     <img src={S1Img} alt="" />
 
