585fc5fe2bd26ad309259f7b9cc000f72cca8940
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-15 04:00:18 +0530

    change header

--- frontend/src/components/common/header.js
+++ frontend/src/components/common/header.js
@@ -12,26 +12,26 @@ const Header = () => {
         <div className="row  header__middle__bar">
           <div className="row container middle__brands">
             <p>BMW</p>
-            <p>Alfa Romeo</p>
-            <p>Cadillac</p>
-            <p>Gensis</p>
-            <p>Tesla</p>
-            <p>Audi</p>
-            <p>Lexus</p>
+            <p>ALFA ROMEO</p>
+            <p>CADDILAC</p>
+            <p>GENSIS</p>
+            <p>TESLA</p>
+            <p>AUDI</p>
+            <p>LEXUS</p>
           </div>
         </div>
         <div className="container nav__container">
           {/* <img src={logo} alt="logo" className="logo" /> */}
-          <nav className="nav">
-            <div className="nav__list">
-              <a href="/" className="nav__link tag__1">
-                Home
+          <nav className="row nav">
+            <div className="row nav__list">
+              <a href="/" className="nav__link ">
+                HOME
               </a>
-              <a href="/about" className="nav__link tag__1">
-                About&nbsp;Us
+              <a href="/about" className="nav__link ">
+                ABOUT&nbsp;US
               </a>
-              <a href="/contact" className="nav__link tag__1">
-                Contact&nbsp;Us
+              <a href="/contact" className="nav__link ">
+                CONTACT&nbsp;US
               </a>
             </div>
           </nav>
