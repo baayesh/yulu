d03dc9929dba26f3e9909b624717946792bcd87c
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-16 11:47:34 +0530

    change font family of the services cards

--- frontend/src/components/home/ourServices.js
+++ frontend/src/components/home/ourServices.js
@@ -7,27 +7,27 @@ const OurServices = () => {
   return (
     <section>
         <div className=" container our__services__container">
-            <h2 className='tag__1'>Our Services</h2>
+            <h2 className=''>Our Services</h2>
             <h4 className='subheading__2'>Our Best Services for You</h4>
             <div className="our__services__cards">
                 <div className="column our__services__card">
                     <img src={S1Img} alt="" />
 
-                    <h3 className='subheading__3'>Service Name</h3>
-                    <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus praesentium, commodi odit dignissimos exercitationem velit. Suscipit labore enim alias, sunt porro debitis et iure quae corporis non asperiores laboriosam quo reprehenderit inventore itaque amet explicabo. Qui doloribus vel possimus </p>
+                    <h3 className=''>Service Name</h3>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus praesentium, commodi odit dignissimos exercitationem velit. Suscipit labore enim alias, sunt porro debitis et iure quae corporis non asperiores laboriosam quo reprehenderit inventore itaque amet explicabo. Qui doloribus vel possimus </p>
                     
          
                 </div>
                 <div className="column our__services__card">
                 <img src={S2Img} alt="" />
-                <h3 className='subheading__3'>Service Name</h3>
-                    <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus praesentium, commodi odit dignissimos exercitationem velit. Suscipit labore enim alias, sunt porro debitis et iure quae corporis non asperiores laboriosam quo reprehenderit inventore itaque amet explicabo. Qui doloribus vel possimus </p>
+                <h3 className=''>Service Name</h3>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus praesentium, commodi odit dignissimos exercitationem velit. Suscipit labore enim alias, sunt porro debitis et iure quae corporis non asperiores laboriosam quo reprehenderit inventore itaque amet explicabo. Qui doloribus vel possimus </p>
                 
                 </div>
                 <div className="column our__services__card">
                 <img src={S3Img} alt="" />
-                <h3 className='subheading__3'>Service Name</h3>
-                    <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus praesentium, commodi odit dignissimos exercitationem velit. Suscipit labore enim alias, sunt porro debitis et iure quae corporis non asperiores laboriosam quo reprehenderit inventore itaque amet explicabo. Qui doloribus vel possimus </p>
+                <h3 className=''>Service Name</h3>
+                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus praesentium, commodi odit dignissimos exercitationem velit. Suscipit labore enim alias, sunt porro debitis et iure quae corporis non asperiores laboriosam quo reprehenderit inventore itaque amet explicabo. Qui doloribus vel possimus </p>
                 </div>
             </div>
         </div>
