320d7265a38ec406a5d4154f6d789a0a58c6417e
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-07-01 07:46:50 +0530

    add links to the footer

--- frontend/src/components/common/footer.js
+++ frontend/src/components/common/footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "../../styles/header__footer.css";
+// Icons
 import { FaFacebookF } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaTiktok } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+// Routings
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -11,9 +14,17 @@ const Footer = () => {
       <div className="footer-main">
         <div className="footer-column">
           <h3>QUICK LINKS</h3>
-          <p>Home</p>
-          <p>About&nbsp;Us</p>
-          <p>Contact&nbsp;Us</p>
+
+          <Link to="/" className="remove__und">
+            <p>Home</p>
+          </Link>
+
+          <Link to="/about" className="remove__und">
+            <p>About&nbsp;Us</p>
+          </Link>
+          <Link to="/contact"  className="remove__und">
+            <p>Contact&nbsp;Us</p>
+          </Link>
           {/* <img src={logo__footer} className="logo-footer" /> */}
         </div>
         <div className="footer-column">
@@ -35,19 +46,31 @@ const Footer = () => {
       </div>
       <div className="footer-column">
         <div className="row footer-social">
-          <div className="social-media-icon-container">
-            <FaFacebookF className="social-media-icon" />
-          </div>
-
-          <div className="social-media-icon-container">
-            <AiFillInstagram className="social-media-icon" />
-          </div>
-          <div className="social-media-icon-container">
-            <FaTiktok className="social-media-icon" />
-          </div>
-          <div className="social-media-icon-container">
-            <FaYoutube className="social-media-icon" />
-          </div>
+      {/* Facebook */}
+            <a href="https://web.facebook.com/nourancollegeshj/" target="_blank">
+              <div className="social-media-icon-container">
+                <FaFacebookF className="social-media-icon" />
+              </div>
+            </a>
+        
+{/* Instagram */}
+          <a href="https://www.instagram.com/yulu_dubai/" target="__blank">
+            <div className="social-media-icon-container">
+              <AiFillInstagram className="social-media-icon" />
+            </div>
+          </a>
+          {/* Tiktok */}
+          <a href="https://www.tiktok.com/@yulurentacar" target="__blank">
+            <div className="social-media-icon-container">
+              <FaTiktok className="social-media-icon" />
+            </div>
+          </a>
+          {/* Youtube */}
+          <a href="https://www.youtube.com/channel/UCFiWhI6goEqqvkoHduE3tfw" target="__blank">
+            <div className="social-media-icon-container">
+              <FaYoutube className="social-media-icon" />
+            </div>
+          </a>
         </div>
       </div>
 
@@ -62,6 +85,12 @@ const Footer = () => {
 
       <div className="footer-lastline">
         <p>&reg;2024 by YULU DUBAI. All Rights Reserved.</p>
+        <h2>
+          Designed and Developed by{" "}
+          <a href="" className="footer__lastline__link">
+            ayeshb
+          </a>
+        </h2>
       </div>
     </div>
   );
